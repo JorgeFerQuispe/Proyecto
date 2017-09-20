9fec608154e20e159f2724d5fc4fe78c5c2d4059
Jorge <JorgeMFQuispe@gmail.com>
2017-09-20 20:11:48 +0200

    Subo mi primer programa java, pero no será el último

---
+++ HolaMundo.java
@@ -0,0 +1,20 @@
+    public class HolaMundo {
+     
+    	public static void main(String[] args) {		
+    		System.out.println("Hola Mundo");
+    	}
+     
+    }
+	
+
+    public class sumar {
+
+	public static void main(String[] args) {
+		int num1 = 3;
+		int num2 = 7;
+		int num3;
+		num3 = num1 + num2;
+			System.out.println(num3);
+	}
+
+    }
